65a2657cf24128285e9fdfa24e58c9b82a21ef7c
Michal Orżanowski <m.orzanowski@student.uw.edu.pl>
2025-03-24 12:08:20 +0100

    cleanup and reorganize

--- wasserstein/__init__.py
+++ wasserstein/__init__.py
@@ -1,5 +1,7 @@
 """
 Wasserstein package initialization.
 """
+from .spectrum import *
+from .nmr_spectrum import *
 
 __version__ = "0.1.0"
---
+++ wasserstein/nmr_spectrum.py
@@ -0,0 +1,28 @@
+from wasserstein import Spectrum
+
+
+class NMRSpectrum(Spectrum):
+    def __init__(
+        self,
+        formula="",
+        threshold=0.001,
+        total_prob=None,
+        charge=1,
+        adduct=None,
+        confs=None,
+        label=None,
+        protons=None,
+        **other,
+    ):
+        Spectrum.__init__(
+            self,
+            formula=formula,
+            threshold=threshold,
+            total_prob=total_prob,
+            charge=charge,
+            adduct=adduct,
+            confs=confs,
+            label=label,
+            **other,
+        )
+        self.protons = protons
